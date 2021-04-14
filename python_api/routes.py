b35f906703bf545714b16a390616cbd478547da4
sven9 <sven9@users.noreply.github.com>
2021-04-13 22:47:24 -0700

    refactor: add `marshmallow`

--- python_api/routes.py
+++ python_api/routes.py
@@ -1,8 +1,12 @@
 from flask import Blueprint
 
+from python_api.schema import LivenessSchema
+
 bp = Blueprint("python_api", __name__)
 
 
 @bp.route("/liveness")
 def liveness():
-    return {"liveness": True}
+    schema = LivenessSchema()
+
+    return schema.dump({"liveness": True})
